3910bbbf4929f3a42ddfb06c9b3436d9e1adafc3
lazzybirdd <rbarinov2004@yahoo.com>
2023-11-13 17:54:32 -0800

    Finished Capstone Project (#2)

--- concert/views.py
+++ concert/views.py
@@ -11,6 +11,9 @@ from concert.models import Concert, ConcertAttending
 import requests as req
 
 
+SONGS_URL = "http://songs-sn-labs-rbarinov2004.labs-prod-openshift-san-a45631dc5778dc6371c67d206ba9ae5c-0000.us-east.containers.appdomain.cloud"
+PHOTO_URL = "https://pictures.19q45lp40fnh.us-south.codeengine.appdomain.cloud"
+
 # Create your views here.
 
 def signup(request):
@@ -35,19 +38,23 @@ def signup(request):
 
 
 def songs(request):
-    songs = {"songs":[{"id":1,"title":"duis faucibus accumsan odio curabitur convallis","lyrics":"Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis."}]}
-    return render(request, "songs.html", {"songs":songs["songs"]})
-
+    # mockup code for testing only
+    # songs = {"songs":[{"id":1,"title":"duis faucibus accumsan odio curabitur convallis","lyrics":"Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis."}]}
+    # return render(request, "songs.html", {"songs":songs["songs"]})
+    songs = req.get(SONGS_URL + "/song").json()
+    return render(request, "songs.html", {"songs": songs["songs"]})
 
 def photos(request):
-    photos = [{
-       "id": 1,
-        "pic_url": "http://dummyimage.com/136x100.png/5fa2dd/ffffff",
-        "event_country": "United States",
-        "event_state": "District of Columbia",
-        "event_city": "Washington",
-        "event_date": "11/16/2022"
-    }]
+    # mockup code for testing only
+    # photos = [{
+    #    "id": 1,
+    #     "pic_url": "http://dummyimage.com/136x100.png/5fa2dd/ffffff",
+    #     "event_country": "United States",
+    #     "event_state": "District of Columbia",
+    #     "event_city": "Washington",
+    #     "event_date": "11/16/2022"
+    # }]
+    photos = req.get(PHOTO_URL + "/picture").json()
     return render(request, "photos.html", {"photos": photos})
 
 
